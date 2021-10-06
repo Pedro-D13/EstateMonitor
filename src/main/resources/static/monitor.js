c84903315dcca28a2f72a7c5689e35500be108d8
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-06 20:09:08 +0100

    implemented dave's feature

--- src/main/resources/static/monitor.js
+++ src/main/resources/static/monitor.js
@@ -1,18 +1,31 @@
+let client = null;
+let clientConnected = false;
 
+// note onMessageHandler will replace previous value
 function subscribeHomeMonitor(topic, onMessageHandler){
-    client = new Paho.MQTT.Client("localhost", 61614, "BrowserClientId");
 
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageHandler;
+    if (client == null) {
+        client = new Paho.MQTT.Client("localhost", 61614, "BrowserClientId");
+        client.onConnectionLost = onConnectionLost;
+        client.onMessageArrived = onMessageHandler;
 
-    client.connect(
-        {onSuccess:onConnect, onFailure: onConnectionFailed}
-    );
+        client.connect(
+            {onSuccess:onConnect, onFailure: onConnectionFailed}
+        );
+    } else {
+        if ( clientConnected ){
+            client.onMessageArrived = onMessageHandler;
+            console.log("already connected, subscribe to " + topic);
+            client.subscribe(topic);
+        }
+    }
+
 
     function onConnect() {
-      // Once a connection has been made, make a subscription and send a message.
-      console.log("onConnect, subscribe to " + topic);
-      client.subscribe(topic);
+        // Once a connection has been made, make a subscription and send a message.
+        console.log("onConnect, subscribe to " + topic);
+        client.subscribe(topic);
+        clientConnected = true;
     };
 
     function onConnectionFailed(responseObject) {
